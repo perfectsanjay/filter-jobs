72b0812e12fe2c245a119631d5f547669ada3869
Sanjay Singh <sanjay007abd@gmail.com>
2025-01-20 21:04:11 +0530

    design the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,63 @@
-import './App.css'
-import data from '/Users/sanjaysingh/Desktop/filtering jobs/data.json'
+import "./App.css";
+import data from "/Users/sanjaysingh/Desktop/filtering jobs/data.json";
 
 function App() {
-  const tag = data.filter((item)=> item.new === true )
-  const feature = data.filter((item) => item.featured===true)
+  const tag = data.filter((item) => item.new === true);
+  const feature = data.filter((item) => item.featured === true);
 
   return (
     <>
-     <div className="container">
-      <img className='header-img' src='images/bg-header-desktop.svg' alt="header_img"/> 
-      <div className="jobCard">
-        {data.map((item) => (
-          <div className="img" key={item.id}>
-              <img src={item.logo} alt="logo" />
-              <div className="job-info">
-                <p className="company">{item.company}</p>
-                {item.new && <span className='newBadge'>NEW!</span>}
-                {item.featured && <span className='featureBadge'>FEATURED</span>}
+      <div className="container">
+        <img
+          className="header-img"
+          src="images/bg-header-desktop.svg"
+          alt="header_img"
+        />
+        <div className="jobCard">
+          <div className="job-sub"></div>
+          {data.map((item) => (
+            <div className="subcont" key={item.id}>
+              <div className="img-info">
+              <div className="img">
+                <img src={item.logo} alt="logo" />
               </div>
-              <p className="profile">{item.position}</p>
-              <div className="time">
-                <p className="postedAt">{item.postedAt}</p>
-                <li className='contract'>{item.contract}</li>
-                <li className='location'>{item.location}</li>
 
+              <div className="info-cont">
+                <div className="job-info">
+                  <p className="company">{item.company}</p>
+                  {item.new && <span className="newBadge">NEW!</span>}
+                  {item.featured &&  <span className="featureBadge">FEATURED</span>}
+                </div>
+                <p className="profile">{item.position}</p>
+                <div className="time">
+                  <p className="postedAt">{item.postedAt}</p>
+                  <li className="contract">{item.contract}</li>
+                  <li className="location">{item.location}</li>
+                </div>
               </div>
+              </div>
+             
+
               <div className="filterButtons">
                 <button className="role">{item.role}</button>
-                <button className='level'>{item.level}</button>
-                {
-                  item.tools.map((item,index) => (
-                    <button className="tooll" key={index}>{item}</button>
-                  ))
-                }
-                {item.languages.map((item,index) => (
-                  <button className="lang" key={index}>{item}</button>
+                <button className="level">{item.level}</button>
+                {item.tools.map((item, index) => (
+                  <button className="tooll" key={index}>
+                    {item}
+                  </button>
                 ))}
-
+                {item.languages.map((item, index) => (
+                  <button className="lang" key={index}>
+                    {item}
+                  </button>
+                ))}
               </div>
-
-          </div>
-         
-       )) }
-
+            </div>
+          ))}
+        </div>
       </div>
-     </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
