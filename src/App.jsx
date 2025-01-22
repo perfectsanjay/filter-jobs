a41381ac2ee2e3ad4e146e635f5691354602524f
Sanjay Singh <sanjay007abd@gmail.com>
2025-01-22 18:15:04 +0530

    complete the functionality of filter the job

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import "./App.css";
 import data from "/Users/sanjaysingh/Desktop/filtering jobs/data.json";
+import { useState } from "react";
 
 function App() {
+  const [selectedFilter,setSelectedFilter] = useState([])
+
+  const handleFilterClick = (filter) => {
+    setSelectedFilter((prevFilters) =>
+      prevFilters.includes(filter)
+        ? prevFilters.filter((item) => item !== filter) 
+        : [...prevFilters, filter] 
+    );
+  };
+  
+  const ClearFilter = () => {
+    setSelectedFilter([])
+  } 
+
+  const filteredJob  = data.filter((item) => {
+    const jobFilter =[
+      item.role,
+      item.level,
+      ...item.tools,
+      ...item.languages,
+    ]
+    return selectedFilter.every((filter) => jobFilter.includes(filter)) 
+  })
 
   return (
     <>
@@ -11,17 +35,24 @@ function App() {
           src="images/bg-header-desktop.svg"
           alt="header_img"
         />
-        <div className="filterBar">
-          <div className="filButton">
-          <p className="filterSelect">Frontend</p>
-          <button className="removeButton">X</button>
-          </div>
-        
-          <button className="clear">clear</button>
-        </div>
+
+        {selectedFilter.length>0 && (
+           <div className="filterBar">
+           <div className="filButton">
+            {selectedFilter.map((filter,index) => (
+           <div className="filterSelect" key={index}><p>{filter}</p>
+           <button className="removeButton" onClick={() => handleFilterClick(filter)}>X</button>
+           </div>
+          ))}
+           </div>
+         
+           <button className="clear" onClick={ClearFilter}>clear</button>
+         </div>
+        )}
+       
         <div className="jobCard">
           <div className="job-sub"></div>
-          {data.map((item) => (
+          {filteredJob.map((item) => (
             <div className="subcont" key={item.id}>
               <div className="img-info">
               <div className="img">
@@ -45,15 +76,15 @@ function App() {
              
 
               <div className="filterButtons">
-                <button className="role">{item.role}</button>
-                <button className="level">{item.level}</button>
+                <button className="role" onClick={() => handleFilterClick(item.role)}>{item.role}</button>
+                <button className="level" onClick={() => handleFilterClick(item.level)}>{item.level}</button>
                 {item.tools.map((item, index) => (
-                  <button className="tooll" key={index}>
+                  <button className="tooll" key={index} onClick={() => handleFilterClick(item)}>
                     {item}
                   </button>
                 ))}
                 {item.languages.map((item, index) => (
-                  <button className="lang" key={index}>
+                  <button className="lang" key={index} onClick={() => handleFilterClick(item)}>
                     {item}
                   </button>
                 ))}
